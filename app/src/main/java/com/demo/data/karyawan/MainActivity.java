c4e6639c2bd3f05bc545fcbadc3968d04467ed0a
raden adhyaksa <raksa69@gmail.com>
2019-07-04 23:59:31 +0700

    Add Webview ProfileCompany

--- app/src/main/java/com/demo/data/karyawan/MainActivity.java
+++ app/src/main/java/com/demo/data/karyawan/MainActivity.java
@@ -3,9 +3,14 @@ package com.demo.data.karyawan;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toolbar;
 
 import com.demo.data.karyawan.menu.AboutActivity;
 import com.demo.data.karyawan.menu.EmployeeActivity;
@@ -13,11 +18,34 @@ import com.demo.data.karyawan.menu.OfficeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //membuat variabel view
+    private WebView view;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = (WebView) this.findViewById(R.id.webview);
+
+
+        view.getSettings().setJavaScriptEnabled(true); //untuk mengaktifkan javascript
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("http://www.multiintisarana.com");
+        view.setWebChromeClient(new WebChromeClient()); //ketika webview selesai load page loading akan dismis
     }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
