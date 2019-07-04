9af88af7651a365fa7118f1f961f3d043bc236e6
raden adhyaksa <raksa69@gmail.com>
2019-07-04 18:26:59 +0700

    provide a Splashscreen function

--- app/src/main/java/com/demo/data/karyawan/SplashScreen.java
+++ app/src/main/java/com/demo/data/karyawan/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.demo.data.karyawan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar(). hide();
+
+        //set timer
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
